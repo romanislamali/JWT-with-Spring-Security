3d60476143d114953052758caac002a4815e2710
Md. Roman Islam Ali <romanislam897@gmail.com>
2023-11-17 23:55:13 +0600

    Create Security Configuration class

---
+++ jwt-with-spring-security/src/main/java/com/roman/jwtwithspringsecurity/config/SecurityConfig.java
@@ -0,0 +1,13 @@
+package com.roman.jwtwithspringsecurity.config;
+
+import com.roman.jwtwithspringsecurity.security.JWTAuthenticationFilter;
+import com.roman.jwtwithspringsecurity.security.JwtAuthenticationEntryPoint;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+}
