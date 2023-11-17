ee4bc833a2cacc9411ad62dcd9c2db350fc373bc
Md. Roman Islam Ali <romanislam897@gmail.com>
2023-11-17 20:38:31 +0600

    Create a list for store users by constructor and create a getUsers method to get all users list.

--- jwt-with-spring-security/src/main/java/com/roman/jwtwithspringsecurity/services/UserService.java
+++ jwt-with-spring-security/src/main/java/com/roman/jwtwithspringsecurity/services/UserService.java
@@ -9,5 +9,16 @@ import java.util.UUID;
 
 @Service
 public class UserService {
+    private List<User> store = new ArrayList<>();
 
+    public UserService() {
+        store.add(new User(UUID.randomUUID().toString(), "Roman Islam", "roman@gmail.com"));
+        store.add(new User(UUID.randomUUID().toString(), "Tajnur Rahman", "tajnur@gmail.com"));
+        store.add(new User(UUID.randomUUID().toString(), "Ehsan habib", "ehsan@gmail.com"));
+        store.add(new User(UUID.randomUUID().toString(), "Seikh Sajib", "sajib@gmail.com"));
+    }
+
+    public List<User> getUsers(){
+        return store;
+    }
 }
