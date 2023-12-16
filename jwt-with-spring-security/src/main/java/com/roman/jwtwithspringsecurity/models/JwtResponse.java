15cb28ec49af0def9c31e96a8a71a07feda7d273
Md. Roman Islam Ali <romanislam897@gmail.com>
2023-12-16 21:50:30 +0600

    JwtResponse update

--- jwt-with-spring-security/src/main/java/com/roman/jwtwithspringsecurity/models/JwtResponse.java
+++ jwt-with-spring-security/src/main/java/com/roman/jwtwithspringsecurity/models/JwtResponse.java
@@ -1,4 +1,15 @@
 package com.roman.jwtwithspringsecurity.models;
 
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
 public class JwtResponse {
+
+    private String jwtToken;
+    private String username;
 }
