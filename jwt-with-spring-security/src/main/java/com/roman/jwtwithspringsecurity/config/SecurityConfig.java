73f8ab8c20b5d9b650edf015e43e97d3b561f08a
Md. Roman Islam Ali <romanislam897@gmail.com>
2023-12-16 21:45:48 +0600

    Security Configuration

--- jwt-with-spring-security/src/main/java/com/roman/jwtwithspringsecurity/config/SecurityConfig.java
+++ jwt-with-spring-security/src/main/java/com/roman/jwtwithspringsecurity/config/SecurityConfig.java
@@ -3,11 +3,31 @@ package com.roman.jwtwithspringsecurity.config;
 import com.roman.jwtwithspringsecurity.security.JWTAuthenticationFilter;
 import com.roman.jwtwithspringsecurity.security.JwtAuthenticationEntryPoint;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
+    @Autowired
+    private JwtAuthenticationEntryPoint point;
+    @Autowired
+    private JWTAuthenticationFilter filter;
 
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.csrf(csrf -> csrf.disable())
+                .cors(cors -> cors.disable())
+                .authorizeHttpRequests(auth -> auth.requestMatchers("/home/**").authenticated().requestMatchers("/auth/login").permitAll().anyRequest().authenticated())
+                .exceptionHandling(ex -> ex.authenticationEntryPoint(point))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+
+        http.addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
 }
